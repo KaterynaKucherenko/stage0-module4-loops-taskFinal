873fe0b8515c30f0a3ae3030d65f131f849b51e8
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-18 23:51:41 +0300

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+
+for (int x = 0; x<=10; x++){
+	        System.out.println(numberTableToPrint + "*" + x + "=" + numberTableToPrint*x);}
+
     }
 }
